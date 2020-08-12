b030b3e063f3d92e22f6a3771ec0ab528e4e50a5
= <=>
2020-08-12 14:51:43 -0300

    Removendo emails

--- config-nodemail.js
+++ config-nodemail.js
@@ -5,8 +5,8 @@ let transporter = nodemailer.createTransport({
     port:'587',
     secure:false,
     auth:{
-        user:'testprod846@gmail.com',
-        pass:'palmeirasganhatudo2020'
+        user:'',
+        pass:''
     },
     tls:{
         rejectUnauthorized:false
